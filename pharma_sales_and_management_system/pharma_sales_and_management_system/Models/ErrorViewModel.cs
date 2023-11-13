e5b2c5e9c30f3f917a62b9fc3a2b62ffb703565c
KartikmPatel <kartikmpatel0804@gmail.com>
2023-11-13 09:37:13 +0530

    changes1

---
+++ pharma_sales_and_management_system/pharma_sales_and_management_system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pharma_sales_and_management_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
